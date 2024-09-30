b5da4cf4e71c75f017173aaf118dfa7af1c0d447
Afaq Ahmed Khan <afaq.qwe@gmail.com>
2024-09-30 12:58:55 +0200

    Adding next starter

---
+++ nextjs/social_network_nextjs15/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"p+Tycjiu3c+DXkEUvKZDGkB/q2zqwrWUaqFQtOv5OQ8=\"\n}"
---
+++ springboot-microservices/inventory-service/inventory-service/target/classes/db/migration/V2__add_inventory.sql
@@ -0,0 +1,6 @@
+INSERT INTO t_inventory (quantity, sku_code)
+VALUES
+  (100, 'iphone_15'),
+  (100, 'iphone_16'),
+  (100, 'iphone_14'),
+  (100, 'iphone_13');
