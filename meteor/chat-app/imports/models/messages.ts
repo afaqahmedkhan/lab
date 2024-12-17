59d867e4bd0ce0832ec173c13a919e6184d7f1c2
Afaq Ahmed Khan <afaq.qwe@gmail.com>
2024-12-17 19:02:26 +0100

    Feat: Add id to the messages collection

--- meteor/chat-app/imports/models/messages.ts
+++ meteor/chat-app/imports/models/messages.ts
@@ -1,6 +1,8 @@
 import { Mongo } from 'meteor/mongo';
+import { Key } from 'react';
 
 export const MessagesCollection = new Mongo.Collection<{
+  _id: Key;
   content: string;
   createdAt: Date;
 }>('messages');
