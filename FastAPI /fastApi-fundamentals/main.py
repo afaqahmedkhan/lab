29a645b967668c96593fc73c74554d5b763b9634
Afaq Ahmed Khan <ak@contexxt.ai>
2025-03-09 00:51:21 +0100

    Feat: FastAPI #1 Installation / API Routes / Swagger Docs

---
+++ FastAPI /fastApi-fundamentals/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def index() -> dict[str,str]:
+    return {'hello': 'world'}
+
+
+@app.get('/about')
+async def about() -> str:
+    return 'an exceptional company'
